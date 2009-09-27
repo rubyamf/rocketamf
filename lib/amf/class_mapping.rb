7be72e575d47f51156e375662debe20615c0d3db
Stephen Augenstein <perl.programmer@gmail.com>
2009-09-27 18:06:40 -0400

    Add RDoc to class mapper

--- lib/amf/class_mapping.rb
+++ lib/amf/class_mapping.rb
@@ -1,40 +1,72 @@
 module AMF
+  # == Class Mapping
+  #
+  # Handles class name mapping between actionscript and ruby and assists in
+  # serializing and deserializing data between them. Simply map an AS class to a
+  # ruby class and when the object is (de)serialized it will end up as the
+  # appropriate class.
+  #
+  # Example:
+  #
+  #   AMF::ClassMapper.define do |m|
+  #     m.map :as => 'AsClass', :ruby => 'RubyClass'
+  #     m.map :as => 'vo.User', :ruby => 'User'
+  #   end
   class ClassMapping
+    # Container for all mapped classes
     class MappingSet
-      def initialize
+      def initialize #:nodoc:
         @as_mappings = {}
         @ruby_mappings = {}
       end
 
+      # Map a given AS class to a ruby class.
+      #
+      # Use fully qualified names for both.
+      #
+      # Example:
+      #
+      #   m.map :as 'com.example.Date', :ruby => 'Example::Date'
       def map params
         [:as, :ruby].each {|k| params[k] = params[k].to_s} # Convert params to strings
         @as_mappings[params[:as]] = params[:ruby]
         @ruby_mappings[params[:ruby]] = params[:as]
       end
 
-      def get_as_class_name class_name
+      # Returns the AS class name for the given ruby class name, returing nil if
+      # not found
+      def get_as_class_name class_name #:nodoc:
         @ruby_mappings[class_name.to_s]
       end
 
-      def get_ruby_class_name class_name
+      # Returns the ruby class name for the given AS class name, returing nil if
+      # not found
+      def get_ruby_class_name class_name #:nodoc:
         @as_mappings[class_name.to_s]
       end
     end
 
+    # Define class mappings in the block
+    #
+    # Example:
+    #
+    #   AMF::ClassMapper.define do |m|
+    #     m.map :as => 'AsClass', :ruby => 'RubyClass'
+    #   end
     def define
       yield mappings
     end
 
-    def get_as_class_name obj
-      if obj.is_a?(String)
-        ruby_class_name = obj
-      else
-        ruby_class_name = obj.class.name
-      end
+    # Returns the AS class name for the given ruby object. Will also take a string
+    # containing the ruby class name
+    def get_as_class_name obj #:nodoc:
+      ruby_class_name = obj.is_a?(String) ? obj : obj.class.name
       mappings.get_as_class_name ruby_class_name
     end
 
-    def populate_ruby_obj as_class_name, props
+    # Creates a ruby object using the mapping configuration based on the source
+    # AS class name. Then, it populates the ruby object using the based in properties
+    def populate_ruby_obj as_class_name, props #:nodoc:
       # Create ruby object
       ruby_class_name = mappings.get_ruby_class_name as_class_name
       if ruby_class_name.nil?
@@ -58,7 +90,9 @@ module AMF
       ruby_obj
     end
 
-    def props_for_serialization ruby_obj
+    # Extracts all exportable properties from the given ruby object and returns
+    # them in a hash
+    def props_for_serialization ruby_obj #:nodoc:
       props = {}
       ruby_obj.public_methods(false).each do |method_name|
         # Add them to the prop hash if they take no arguments
