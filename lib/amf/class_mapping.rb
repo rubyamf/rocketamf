3ab207c47a060a65a391e0993ef598d6ff5a13ac
Stephen Augenstein <perl.programmer@gmail.com>
2009-09-27 17:39:00 -0400

    Add (de)serialization of ruby object properties to class mapper

--- lib/amf/class_mapping.rb
+++ lib/amf/class_mapping.rb
@@ -25,6 +25,49 @@ module AMF
       yield mappings
     end
 
+    def get_as_class_name obj
+      if obj.is_a?(String)
+        ruby_class_name = obj
+      else
+        ruby_class_name = obj.class.name
+      end
+      mappings.get_as_class_name ruby_class_name
+    end
+
+    def populate_ruby_obj as_class_name, props
+      # Create ruby object
+      ruby_class_name = mappings.get_ruby_class_name as_class_name
+      if ruby_class_name.nil?
+        # Populate a simple hash, since no mapping
+        ruby_obj = {}
+      else
+        ruby_class = ruby_class_name.split('::').inject(Kernel) {|scope, const_name| scope.const_get(const_name)}
+        ruby_obj = ruby_class.new
+      end
+
+      # Populate
+      props.each do |key, value|
+        if ruby_obj.respond_to?("#{key}=")
+          ruby_obj.send("#{key}=", value)
+        elsif ruby_obj.respond_to?("[]=")
+          ruby_obj[key.to_s] = value
+        end
+      end
+
+      # Return object
+      ruby_obj
+    end
+
+    def props_for_serialization ruby_obj
+      props = {}
+      ruby_obj.public_methods(false).each do |method_name|
+        # Add them to the prop hash if they take no arguments
+        method_def = ruby_obj.method(method_name)
+        props[method_name.to_s] = ruby_obj.send(method_name) if method_def.arity == 0
+      end
+      props
+    end
+
     private
     def mappings
       @mappings ||= MappingSet.new
--- spec/class_mapping_spec.rb
+++ spec/class_mapping_spec.rb
@@ -2,24 +2,51 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 
 describe AMF::ClassMapping do
   before(:all) do
-    # So we can get at the config object
-    class AMF::ClassMapping
-      attr_accessor :config
+    class RubyClass
+      attr_accessor :prop_a
+      attr_accessor :prop_b
+      attr_accessor :prop_c
     end
   end
 
   before :each do
     @mapper = AMF::ClassMapping.new
+    @mapper.define do |m|
+      m.map :as => 'ASClass', :ruby => 'RubyClass'
+    end
+  end
+
+  it "should return AS class name for ruby objects" do
+    @mapper.get_as_class_name(RubyClass.new).should == 'ASClass'
+    @mapper.get_as_class_name('RubyClass').should == 'ASClass'
   end
 
   it "should allow config modification" do
-    @mapper.define do |c|
-      c.map :as => 'FirstClass', :ruby => 'RubyClass'
+    @mapper.define do |m|
+      m.map :as => 'SecondClass', :ruby => 'RubyClass'
     end
-    @mapper.define do |c|
-      c.map :as => 'SecondClass', :ruby => 'RubyClass'
+    @mapper.get_as_class_name(RubyClass.new).should == 'SecondClass'
+  end
+
+  describe "ruby object populator" do
+    it "should populate ruby objects from AS data" do
+      obj = @mapper.populate_ruby_obj 'ASClass', {:prop_a => 'Data'}
+      obj.prop_a.should == 'Data'
     end
 
-    @mapper.config.get_as_class_name('RubyClass').should == 'SecondClass'
+    it "should populate a hash if no mapping" do
+      obj = @mapper.populate_ruby_obj 'BadClass', {:prop_a => 'Data'}
+      obj.should be_a(Hash)
+      obj['prop_a'].should == 'Data'
+    end
+  end
+
+  it "should extract props for serialization" do
+    obj = RubyClass.new
+    obj.prop_a = 'Test A'
+    obj.prop_b = 'Test B'
+
+    hash = @mapper.props_for_serialization obj
+    hash.should == {'prop_a' => 'Test A', 'prop_b' => 'Test B', 'prop_c' => nil}
   end
 end
