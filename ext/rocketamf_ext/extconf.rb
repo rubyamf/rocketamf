9e80f0a08c97b75386552b8c760cc4f7b3db2df9
Stephen Augenstein <perl.programmer@gmail.com>
2011-10-15 12:27:36 -0400

    Detect JRuby in extconf.rb and don't compile the native extensions in that case. Fixes #7.

--- ext/rocketamf_ext/extconf.rb
+++ ext/rocketamf_ext/extconf.rb
@@ -1,5 +1,13 @@
 require 'mkmf'
 
+# Disable the native extension by creating an empty Makefile on JRuby
+if defined? JRUBY_VERSION
+  message "Generating phony Makefile for JRuby so the gem installs"
+  mfile = File.join(File.dirname(__FILE__), 'Makefile')
+  File.open(mfile, 'w') {|f| f.write dummy_makefile(File.dirname(__FILE__)) }
+  exit 0
+end
+
 if enable_config("sort-props", false)
   $defs.push("-DSORT_PROPS") unless $defs.include? "-DSORT_PROPS"
 end
